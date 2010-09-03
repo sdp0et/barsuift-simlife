8d209d3ebdb82161a989af893a02310fdbf20c3e
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-09-03 23:05:34 +0000

    order the TODO

--- trunk/simLifeUtilities/src/main/java/barsuift/simLife/time/SimLifeCalendar.java
+++ trunk/simLifeUtilities/src/main/java/barsuift/simLife/time/SimLifeCalendar.java
@@ -22,7 +22,7 @@ import java.text.MessageFormat;
 import java.util.Calendar;
 import java.util.regex.Pattern;
 
-// TODO 006. use the calendar system
+// TODO 009. use the calendar system
 public class SimLifeCalendar extends Calendar {
 
     private static final long serialVersionUID = -2239086430259505817L;
