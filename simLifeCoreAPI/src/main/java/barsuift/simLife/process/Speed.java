737ba1fffb037afcc97ffa415c084c45b5d3e8f3
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-05 10:01:37 +0000

    javadoc

--- simLifeCoreAPI/src/main/java/barsuift/simLife/process/Speed.java
+++ simLifeCoreAPI/src/main/java/barsuift/simLife/process/Speed.java
@@ -3,8 +3,19 @@ package barsuift.simLife.process;
 
 public enum Speed {
 
+    /**
+     * Normal speed of 1
+     */
     NORMAL(1),
+
+    /**
+     * Fast speed of 5
+     */
     FAST(5),
+
+    /**
+     * Very fast speed of 20
+     */
     VERY_FAST(20);
 
     private final int speed;
