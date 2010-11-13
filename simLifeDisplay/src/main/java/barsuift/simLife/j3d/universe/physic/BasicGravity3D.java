7342a914f97b01749f71b7ac1bed85ae718822b5
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-13 23:37:58 +0000

    the gravity3D is now able to schedule the GravityTask, and add to it the falling leaves

--- simLifeDisplay/src/main/java/barsuift/simLife/j3d/universe/physic/BasicGravity3D.java
+++ simLifeDisplay/src/main/java/barsuift/simLife/j3d/universe/physic/BasicGravity3D.java
@@ -3,6 +3,12 @@ package barsuift.simLife.j3d.universe.physic;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Group;
 
+import barsuift.simLife.j3d.universe.Universe3D;
+import barsuift.simLife.process.ConditionalTaskStateFactory;
+import barsuift.simLife.process.GravityTask;
+import barsuift.simLife.process.SplitConditionalTaskState;
+import barsuift.simLife.process.Synchronizer3D;
+
 
 public class BasicGravity3D implements Gravity3D {
 
@@ -10,9 +16,18 @@ public class BasicGravity3D implements Gravity3D {
 
     private final Group group;
 
-    public BasicGravity3D(Gravity3DState state) {
+    private GravityTask gravityTask;
+
+    public BasicGravity3D(Gravity3DState state, Universe3D universe3D) {
         this.state = state;
         group = new BranchGroup();
+        group.setCapability(Group.ALLOW_CHILDREN_EXTEND);
+        ConditionalTaskStateFactory taskFactory = new ConditionalTaskStateFactory();
+        Synchronizer3D synchronizer3D = universe3D.getSynchronizer();
+        SplitConditionalTaskState gravityTaskState = taskFactory.createSplitConditionalTaskState(GravityTask.class,
+                synchronizer3D.getStepSize());
+        gravityTask = new GravityTask(gravityTaskState);
+        synchronizer3D.schedule(gravityTask);
     }
 
     @Override
@@ -22,7 +37,8 @@ public class BasicGravity3D implements Gravity3D {
 
     @Override
     public void fall(BranchGroup groupToFall) {
-        // put the fallen leaves and interpolator in this group
+        gravityTask.fall(groupToFall);
+        group.addChild(groupToFall);
     }
 
     @Override
