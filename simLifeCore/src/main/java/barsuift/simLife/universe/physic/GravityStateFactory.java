fe0a6a5f657d9c2c0167c85acb924039531f5ca3
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-13 19:37:42 +0000

    add falling leaves

--- simLifeCore/src/main/java/barsuift/simLife/universe/physic/GravityStateFactory.java
+++ simLifeCore/src/main/java/barsuift/simLife/universe/physic/GravityStateFactory.java
@@ -1,7 +1,11 @@
 package barsuift.simLife.universe.physic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import barsuift.simLife.j3d.universe.physic.Gravity3DState;
 import barsuift.simLife.j3d.universe.physic.Gravity3DStateFactory;
+import barsuift.simLife.tree.TreeLeafState;
 
 
 public class GravityStateFactory {
@@ -9,7 +13,8 @@ public class GravityStateFactory {
     public GravityState createGravityState() {
         Gravity3DStateFactory gravity3DFactory = new Gravity3DStateFactory();
         Gravity3DState gravity3D = gravity3DFactory.createGravity3DState();
-        return new GravityState(gravity3D);
+        Set<TreeLeafState> fallingLeaves = new HashSet<TreeLeafState>();
+        return new GravityState(gravity3D, fallingLeaves);
     }
 
 }
