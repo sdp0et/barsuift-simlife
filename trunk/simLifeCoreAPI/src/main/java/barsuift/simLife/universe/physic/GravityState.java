c645971ec950ae7472f3a45cb6026fc41710c648
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-13 19:38:56 +0000

    add falling leaves

--- trunk/simLifeCoreAPI/src/main/java/barsuift/simLife/universe/physic/GravityState.java
+++ trunk/simLifeCoreAPI/src/main/java/barsuift/simLife/universe/physic/GravityState.java
@@ -1,23 +1,31 @@
 package barsuift.simLife.universe.physic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 import barsuift.simLife.State;
 import barsuift.simLife.j3d.universe.physic.Gravity3DState;
+import barsuift.simLife.tree.TreeLeafState;
 
 @XmlRootElement
 public class GravityState implements State {
 
     private Gravity3DState gravity3D;
 
+    private Set<TreeLeafState> fallingLeaves;
+
     public GravityState() {
         super();
         gravity3D = new Gravity3DState();
+        this.fallingLeaves = new HashSet<TreeLeafState>();
     }
 
-    public GravityState(Gravity3DState gravity3D) {
+    public GravityState(Gravity3DState gravity3D, Set<TreeLeafState> fallingLeaves) {
         super();
         this.gravity3D = gravity3D;
+        this.fallingLeaves = fallingLeaves;
     }
 
     public Gravity3DState getGravity3D() {
@@ -28,11 +36,20 @@ public class GravityState implements State {
         gravity3D = gravity3d;
     }
 
+    public Set<TreeLeafState> getFallingLeaves() {
+        return fallingLeaves;
+    }
+
+    public void setFallingLeaves(Set<TreeLeafState> fallingLeaves) {
+        this.fallingLeaves = fallingLeaves;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((gravity3D == null) ? 0 : gravity3D.hashCode());
+        result = prime * result + ((fallingLeaves == null) ? 0 : fallingLeaves.hashCode());
         return result;
     }
 
@@ -51,12 +68,18 @@ public class GravityState implements State {
         } else
             if (!gravity3D.equals(other.gravity3D))
                 return false;
+        if (fallingLeaves == null) {
+            if (other.fallingLeaves != null)
+                return false;
+        } else
+            if (!fallingLeaves.equals(other.fallingLeaves))
+                return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "GravityState [gravity3D=" + gravity3D + "]";
+        return "GravityState [gravity3D=" + gravity3D + ", fallingLeaves=" + fallingLeaves + "]";
     }
 
 }
