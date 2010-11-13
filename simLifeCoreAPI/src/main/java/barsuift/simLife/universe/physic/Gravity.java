751359f1a3ce54f3a7efac70b11b9c59dc173cb1
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-13 19:38:56 +0000

    add falling leaves

--- simLifeCoreAPI/src/main/java/barsuift/simLife/universe/physic/Gravity.java
+++ simLifeCoreAPI/src/main/java/barsuift/simLife/universe/physic/Gravity.java
@@ -1,11 +1,23 @@
 package barsuift.simLife.universe.physic;
 
+import java.util.Set;
+
 import barsuift.simLife.Persistent;
 import barsuift.simLife.j3d.universe.physic.Gravity3D;
+import barsuift.simLife.tree.TreeLeaf;
 
 
 public interface Gravity extends Persistent<GravityState> {
 
     public Gravity3D getGravity3D();
+
+    /**
+     * Return an unmodifiable Set of falling leaves
+     * 
+     * @return the falling leaves
+     */
+    public Set<TreeLeaf> getFallingLeaves();
+
+    public void addFallingLeaf(TreeLeaf treeLeaf);
 
 }
