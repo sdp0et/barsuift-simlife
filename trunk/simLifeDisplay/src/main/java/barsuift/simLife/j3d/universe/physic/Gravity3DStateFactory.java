62dea369f21de3d878bbfa150f7ab1cf3dd9fa09
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-15 12:35:30 +0000

    add ConditionalTaskState for gravityTask

--- trunk/simLifeDisplay/src/main/java/barsuift/simLife/j3d/universe/physic/Gravity3DStateFactory.java
+++ trunk/simLifeDisplay/src/main/java/barsuift/simLife/j3d/universe/physic/Gravity3DStateFactory.java
@@ -1,10 +1,16 @@
 package barsuift.simLife.j3d.universe.physic;
 
+import barsuift.simLife.process.ConditionalTaskStateFactory;
+import barsuift.simLife.process.GravityTask;
+import barsuift.simLife.process.SplitConditionalTaskState;
+
 
 public class Gravity3DStateFactory {
 
     public Gravity3DState createGravity3DState() {
-        return new Gravity3DState();
+        ConditionalTaskStateFactory taskFactory = new ConditionalTaskStateFactory();
+        SplitConditionalTaskState gravityTaskState = taskFactory.createSplitConditionalTaskState(GravityTask.class);
+        return new Gravity3DState(gravityTaskState);
     }
 
 }
