3430c393db0a6087c618f955603bf31212aff9be
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-15 13:27:13 +0000

    add unit test

--- simLifeCore/src/test/java/barsuift/simLife/universe/physic/BasicGravityTest.java
+++ simLifeCore/src/test/java/barsuift/simLife/universe/physic/BasicGravityTest.java
@@ -1,7 +1,12 @@
 package barsuift.simLife.universe.physic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import junit.framework.TestCase;
+import barsuift.simLife.CoreDataCreatorForTests;
 import barsuift.simLife.tree.MockTreeLeaf;
+import barsuift.simLife.tree.TreeLeafState;
 import barsuift.simLife.universe.BasicUniverse;
 import barsuift.simLife.universe.UniverseState;
 import barsuift.simLife.universe.UniverseStateFactory;
@@ -9,21 +14,42 @@ import barsuift.simLife.universe.UniverseStateFactory;
 
 public class BasicGravityTest extends TestCase {
 
+    private BasicUniverse universe;
+
+    private GravityState gravityState;
+
     protected void setUp() throws Exception {
         super.setUp();
+        UniverseStateFactory universeStateFactory = new UniverseStateFactory();
+        UniverseState universeState = universeStateFactory.createRandomUniverseState();
+        universe = new BasicUniverse(universeState);
+
+        GravityStateFactory gravityStateFactory = new GravityStateFactory();
+        gravityState = gravityStateFactory.createGravityState();
     }
 
     protected void tearDown() throws Exception {
         super.tearDown();
+        universe = null;
+        gravityState = null;
+    }
+
+    public void testAddFallingLeaf() {
+        Set<TreeLeafState> fallingLeaves = new HashSet<TreeLeafState>();
+        fallingLeaves.add(CoreDataCreatorForTests.createRandomTreeLeafState());
+        fallingLeaves.add(CoreDataCreatorForTests.createRandomTreeLeafState());
+        gravityState.setFallingLeaves(fallingLeaves);
+
+        BasicGravity gravity = new BasicGravity(gravityState, universe);
+        assertEquals(2, gravity.getFallingLeaves().size());
+        assertEquals(2, gravity.getGravity3D().getGroup().numChildren());
+
+        gravity.addFallingLeaf(new MockTreeLeaf());
+        assertEquals(3, gravity.getFallingLeaves().size());
+        assertEquals(3, gravity.getGravity3D().getGroup().numChildren());
     }
 
     public void testGetState() {
-        UniverseStateFactory universeStateFactory = new UniverseStateFactory();
-        UniverseState universeState = universeStateFactory.createRandomUniverseState();
-        BasicUniverse universe = new BasicUniverse(universeState);
-
-        GravityStateFactory gravityStateFactory = new GravityStateFactory();
-        GravityState gravityState = gravityStateFactory.createGravityState();
         BasicGravity gravity = new BasicGravity(gravityState, universe);
 
         assertEquals(gravityState, gravity.getState());
