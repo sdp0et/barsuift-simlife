84a1f0f0c0e46cc4511297042351d921d29eed76
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-15 12:35:30 +0000

    add ConditionalTaskState for gravityTask

--- simLifeDisplay/src/main/java/barsuift/simLife/j3d/universe/physic/BasicGravity3D.java
+++ simLifeDisplay/src/main/java/barsuift/simLife/j3d/universe/physic/BasicGravity3D.java
@@ -4,9 +4,7 @@ import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Group;
 
 import barsuift.simLife.j3d.universe.Universe3D;
-import barsuift.simLife.process.ConditionalTaskStateFactory;
 import barsuift.simLife.process.GravityTask;
-import barsuift.simLife.process.SplitConditionalTaskState;
 import barsuift.simLife.process.Synchronizer3D;
 
 
@@ -22,11 +20,9 @@ public class BasicGravity3D implements Gravity3D {
         this.state = state;
         group = new BranchGroup();
         group.setCapability(Group.ALLOW_CHILDREN_EXTEND);
-        ConditionalTaskStateFactory taskFactory = new ConditionalTaskStateFactory();
         Synchronizer3D synchronizer3D = universe3D.getSynchronizer();
-        SplitConditionalTaskState gravityTaskState = taskFactory.createSplitConditionalTaskState(GravityTask.class,
-                synchronizer3D.getStepSize());
-        gravityTask = new GravityTask(gravityTaskState);
+        gravityTask = new GravityTask(state.getGravityTask());
+        gravityTask.setStepSize(synchronizer3D.getStepSize());
         synchronizer3D.schedule(gravityTask);
     }
 
@@ -47,9 +43,10 @@ public class BasicGravity3D implements Gravity3D {
         return state;
     }
 
+    // FIXME unit test
     @Override
     public void synchronize() {
-        // nothing to do
+        gravityTask.synchronize();
     }
 
 }
