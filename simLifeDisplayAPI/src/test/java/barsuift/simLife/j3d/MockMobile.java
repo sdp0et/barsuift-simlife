e2abc7c7cd5e3085a5d95b36a7bf04d1ae227a20
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-11-16 10:59:04 +0000

    improve mock publishing behavior

--- simLifeDisplayAPI/src/test/java/barsuift/simLife/j3d/MockMobile.java
+++ simLifeDisplayAPI/src/test/java/barsuift/simLife/j3d/MockMobile.java
@@ -4,23 +4,25 @@ import javax.media.j3d.BranchGroup;
 import javax.media.j3d.TransformGroup;
 
 import barsuift.simLife.message.BasicPublisher;
+import barsuift.simLife.message.Publisher;
+import barsuift.simLife.message.Subscriber;
 
 
-public class MockMobile extends BasicPublisher implements Mobile {
+public class MockMobile implements Mobile {
 
     private BranchGroup bg;
 
     private TransformGroup tg;
 
+    private final Publisher publisher = new BasicPublisher(this);
 
     public MockMobile() {
-        super(null);
         reset();
     }
 
     public void reset() {
-        BranchGroup bg = new BranchGroup();
-        TransformGroup tg = new TransformGroup();
+        bg = new BranchGroup();
+        tg = new TransformGroup();
         bg.addChild(tg);
 
     }
@@ -45,6 +47,43 @@ public class MockMobile extends BasicPublisher implements Mobile {
         this.bg.removeChild(tg);
         this.bg = bg;
         bg.addChild(tg);
+    }
+
+
+    public void addSubscriber(Subscriber subscriber) {
+        publisher.addSubscriber(subscriber);
+    }
+
+    public void deleteSubscriber(Subscriber subscriber) {
+        publisher.deleteSubscriber(subscriber);
+    }
+
+    public void notifySubscribers() {
+        publisher.notifySubscribers();
+    }
+
+    public void notifySubscribers(Object arg) {
+        publisher.notifySubscribers(arg);
+    }
+
+    public void deleteSubscribers() {
+        publisher.deleteSubscribers();
+    }
+
+    public boolean hasChanged() {
+        return publisher.hasChanged();
+    }
+
+    public int countSubscribers() {
+        return publisher.countSubscribers();
+    }
+
+    public void setChanged() {
+        publisher.setChanged();
+    }
+
+    public void clearChanged() {
+        publisher.clearChanged();
     }
 
 }
