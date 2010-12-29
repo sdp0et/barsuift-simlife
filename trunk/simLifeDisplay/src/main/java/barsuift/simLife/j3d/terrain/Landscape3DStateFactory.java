136f9792f6c13f1306b900f52e1a45190df2510d
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-12-29 12:41:23 +0000

    add random terrain generation

--- trunk/simLifeDisplay/src/main/java/barsuift/simLife/j3d/terrain/Landscape3DStateFactory.java
+++ trunk/simLifeDisplay/src/main/java/barsuift/simLife/j3d/terrain/Landscape3DStateFactory.java
@@ -18,10 +18,64 @@
  */
 package barsuift.simLife.j3d.terrain;
 
+import java.util.Arrays;
+
 public class Landscape3DStateFactory {
 
-    public Landscape3DState createLandscape3DState() {
-        return new Landscape3DState();
+    public Landscape3DState createLandscape3DState() throws Exception {
+        // TODO 001. size should be a parameter, as well as roughness
+        int size = 64;
+        int roughness = 1;
+        MidPointHeightMapGenerator generator = new MidPointHeightMapGenerator(size, roughness);
+        float[] coordinates = generator.getHeightData();
+        int[] coordinatesIndices = generateCoordinatesIndices(size);
+        int[] stripCounts = generateStripCounts(size);
+        return new Landscape3DState(size, coordinates, coordinatesIndices, stripCounts);
+    }
+
+    /**
+     * Generates coordinates indices for triangles. It considers a square of points aligned from left to right, and from
+     * top to bottom. It considers each minimal square of four points, and split it into 2 triangles. It then add to the
+     * result array the coordinates of these 2 triangles (6 coordinates indices).
+     * 
+     * @param size the size of the landscape square array (size = number of lines = number of columns)
+     * @return an array of triangle points indices
+     */
+    // TODO 000. unit test this method
+    // TODO use this : int[] coordIdx = new int[] { 0, 4, 1, 1, 4, 5, 4, 8, 5, 5, 8, 9, 8, 12, 9, 9, 12, 13, 1, 5, 2, 2,
+    // 5, 6, 5, 9,
+    // 6, 6, 9, 10, 9, 13, 10, 10, 13, 14, 2, 6, 3, 3, 6, 7, 6, 10, 7, 7, 10, 11, 10, 14, 11, 11, 14, 15 };
+    private int[] generateCoordinatesIndices(int size) {
+        // 2 triangles * 3 points * (size-1) on each line * (size-1) lines
+        int resultSize = 2 * 3 * (size - 1) * (size - 1);
+        int[] result = new int[resultSize];
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                result[(i * (size - 1) + j) * 6 + 0] = i * size + j;
+                result[(i * (size - 1) + j) * 6 + 1] = i * size + j + size;
+                result[(i * (size - 1) + j) * 6 + 2] = i * size + j + 1;
+
+                result[(i * (size - 1) + j) * 6 + 3] = i * size + j + 1;
+                result[(i * (size - 1) + j) * 6 + 4] = i * size + j + size;
+                result[(i * (size - 1) + j) * 6 + 5] = i * size + j + size + 1;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Consider 2 triangles for each minimal square of the landscape.
+     * 
+     * @param size the size of the landscape square array (size = number of lines = number of columns)
+     * @return the strip counts array
+     */
+    // TODO 000. unit test this method
+    private int[] generateStripCounts(int size) {
+        // 2 triangles * (size-1) on each line * (size-1) lines
+        int stripCountSize = 2 * (size - 1) * (size - 1);
+        int[] stripCounts = new int[stripCountSize];
+        Arrays.fill(stripCounts, 3);
+        return stripCounts;
     }
 
 }
