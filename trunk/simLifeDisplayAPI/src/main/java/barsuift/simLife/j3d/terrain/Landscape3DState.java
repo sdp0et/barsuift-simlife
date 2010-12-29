d14d4513ee5c8a860bb430126006817c0f7d8148
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2010-12-29 12:34:04 +0000

    add size, coordinates, coordinatesIndices, and stripCounts to state object

--- trunk/simLifeDisplayAPI/src/main/java/barsuift/simLife/j3d/terrain/Landscape3DState.java
+++ trunk/simLifeDisplayAPI/src/main/java/barsuift/simLife/j3d/terrain/Landscape3DState.java
@@ -18,6 +18,8 @@
  */
 package barsuift.simLife.j3d.terrain;
 
+import java.util.Arrays;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 import barsuift.simLife.State;
@@ -25,12 +27,68 @@ import barsuift.simLife.State;
 @XmlRootElement
 public class Landscape3DState implements State {
 
+    private int size;
+
+    private float[] coordinates;
+
+    private int[] coordinatesIndices;
+
+    private int[] stripCounts;
+
     public Landscape3DState() {
+        this.size = 0;
+        this.coordinates = new float[0];
+        this.coordinatesIndices = new int[0];
+        this.stripCounts = new int[0];
+    }
+
+    public Landscape3DState(int size, float[] coordinates, int[] coordinatesIndices, int[] stripCounts) {
+        this.size = size;
+        this.coordinates = coordinates;
+        this.coordinatesIndices = coordinatesIndices;
+        this.stripCounts = stripCounts;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public float[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(float[] coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public int[] getCoordinatesIndices() {
+        return coordinatesIndices;
+    }
+
+    public void setCoordinatesIndices(int[] coordinatesIndices) {
+        this.coordinatesIndices = coordinatesIndices;
+    }
+
+    public int[] getStripCounts() {
+        return stripCounts;
+    }
+
+    public void setStripCounts(int[] stripCounts) {
+        this.stripCounts = stripCounts;
     }
 
     @Override
     public int hashCode() {
+        final int prime = 31;
         int result = 1;
+        result = prime * result + Arrays.hashCode(coordinates);
+        result = prime * result + Arrays.hashCode(coordinatesIndices);
+        result = prime * result + size;
+        result = prime * result + Arrays.hashCode(stripCounts);
         return result;
     }
 
@@ -42,12 +100,23 @@ public class Landscape3DState implements State {
             return false;
         if (getClass() != obj.getClass())
             return false;
+        Landscape3DState other = (Landscape3DState) obj;
+        if (!Arrays.equals(coordinates, other.coordinates))
+            return false;
+        if (!Arrays.equals(coordinatesIndices, other.coordinatesIndices))
+            return false;
+        if (size != other.size)
+            return false;
+        if (!Arrays.equals(stripCounts, other.stripCounts))
+            return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Landscape3DState []";
+        return "Landscape3DState [size=" + size + ", coordinates=" + Arrays.toString(coordinates)
+                + ", coordinatesIndices=" + Arrays.toString(coordinatesIndices) + ", stripCounts="
+                + Arrays.toString(stripCounts) + "]";
     }
 
 }
