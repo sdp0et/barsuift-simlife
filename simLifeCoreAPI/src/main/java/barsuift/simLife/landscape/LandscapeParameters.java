8bbd7ca366899cfb80071df5a6fe5600a9e05641
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2011-01-04 17:15:55 +0000

    update TODO

--- simLifeCoreAPI/src/main/java/barsuift/simLife/landscape/LandscapeParameters.java
+++ simLifeCoreAPI/src/main/java/barsuift/simLife/landscape/LandscapeParameters.java
@@ -45,10 +45,8 @@ package barsuift.simLife.landscape;
  * </ul>
  * 
  */
-// TODO 001. create factory class to create random and default.
-// TODO 002. create a hierarchy of Parameters classes (World, Landscape, Trees, ...). Think about where the "size"
-// parameters should go as it is used by many parameters and factory classes.
-// TODO 003. all the methods should be with parameters (in StateFactory classes; So remove the one without parameters,
+// TODO 001. create factory class to create random and default, and add the appropriate buttons.
+// TODO 003. all the methods should be with parameters (in StateFactory classes). So remove the one without parameters,
 // and rename the one with parameters by deleting the mention "WithParamters"in their names
 public class LandscapeParameters {
 
