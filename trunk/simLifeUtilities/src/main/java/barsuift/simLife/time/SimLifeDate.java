31b44fcb76ec5b7f1a83a9217a7052caacc4d2c4
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2011-04-07 08:22:12 +0000

    javadoc

--- trunk/simLifeUtilities/src/main/java/barsuift/simLife/time/SimLifeDate.java
+++ trunk/simLifeUtilities/src/main/java/barsuift/simLife/time/SimLifeDate.java
@@ -94,21 +94,42 @@ public class SimLifeDate implements Persistent<SimLifeDateState>, Publisher {
         this(0);
     }
 
+    /**
+     * Creates a new date by parsing the given string. The parsing is done by the method {@link #setTime(String)}.
+     * 
+     * @param date the formatted date
+     * @throws ParseException if the date is not in the appropriate format
+     */
     public SimLifeDate(String date) throws ParseException {
         setTime(date);
         this.state = new SimLifeDateState(getTimeInMillis());
     }
 
+    /**
+     * Creates a new date with the given milliseconds.
+     * 
+     * @param millis the milliseconds
+     */
     public SimLifeDate(long millis) {
         setTimeInMillis(millis);
         computeFields();
         this.state = new SimLifeDateState(millis);
     }
 
+    /**
+     * Duplicated the given date instance.
+     * 
+     * @param copy the date to copy
+     */
     public SimLifeDate(SimLifeDate copy) {
         this(copy.getTimeInMillis());
     }
 
+    /**
+     * Creates a new date from the given date state.
+     * 
+     * @param dateState the date state
+     */
     public SimLifeDate(SimLifeDateState dateState) {
         if (dateState == null) {
             throw new IllegalArgumentException("date state is null");
@@ -128,42 +149,90 @@ public class SimLifeDate implements Persistent<SimLifeDateState>, Publisher {
         state.setValue(getTimeInMillis());
     }
 
+    /**
+     * Returns the time in milliseconds. The milliseconds are always superior or equal to 0.
+     * 
+     * @return the time in milliseconds
+     */
     public long getTimeInMillis() {
         return timeInMillis;
     }
 
+    /**
+     * Return the milliseconds in the current second, in the range between 0 and 999
+     * 
+     * @return the millisecond part of the current date
+     */
     public int getMillisOfSecond() {
         return millisOfSecond;
     }
 
+    /**
+     * Return the seconds in the current minute, in the range between 0 and 59
+     * 
+     * @return the second part of the current date
+     */
     public int getSecondOfMinute() {
         return secondOfMinute;
     }
 
+    /**
+     * Return the minutes in the current day, in the range between 0 and 19
+     * 
+     * @return the minute part of the current date
+     */
     public int getMinuteOfDay() {
         return minuteOfDay;
     }
 
+    /**
+     * Return the day in the current week
+     * 
+     * @return the week day part of the current date
+     */
     public Day getDayOfWeek() {
         return dayOfWeek;
     }
 
+    /**
+     * Return the day in the current month, in the range between 1 and 18
+     * 
+     * @return the month day part of the current date
+     */
     public int getDayOfMonth() {
         return (dayOfWeek.getIndex()) + ((weekOfMonth - 1) * DAY_PER_WEEK);
     }
 
+    /**
+     * Return the week in the current month, in the range between 1 and 3
+     * 
+     * @return the month week part of the current date
+     */
     public int getWeekOfMonth() {
         return weekOfMonth;
     }
 
+    /**
+     * Return the month in the current year
+     * 
+     * @return the month part of the current date
+     */
     public Month getMonthOfYear() {
         return monthOfYear;
     }
 
+    /**
+     * Return the year, always superior or equal to 0.
+     * 
+     * @return the year part of the current date
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * Computes the {@link #timeInMillis} field from other fields.
+     */
     private void computeTime() {
         long result = year - 1; // time in years
         result = result * MONTH_PER_YEAR + monthOfYear.getIndex() - 1; // time in months
@@ -177,6 +246,9 @@ public class SimLifeDate implements Persistent<SimLifeDateState>, Publisher {
         notifySubscribers();
     }
 
+    /**
+     * Computes all the other fields from the {@link #timeInMillis} field.
+     */
     private void computeFields() {
         // number of complete days in this time
         long absDay = timeInMillis / MS_FOR_ONE_DAY;
@@ -192,11 +264,27 @@ public class SimLifeDate implements Persistent<SimLifeDateState>, Publisher {
         notifySubscribers();
     }
 
+    /**
+     * Set the time from the given milliseconds. All the other fields are updated accordingly.
+     * 
+     * @param millis the time in milliseconds
+     */
     public synchronized void setTimeInMillis(long millis) {
         timeInMillis = millis;
         computeFields();
     }
 
+    /**
+     * Set all the date fields
+     * 
+     * @param millisOfSecond the milliseconds in the range between 0 and 999
+     * @param secondOfMinute the seconds in the range between 0 and 59
+     * @param minuteOfDay the minutes in the range between 0 and 19
+     * @param dayOfWeek the day of week
+     * @param weekOfMonth the week of month in the range between 1 and 3
+     * @param monthOfYear the month
+     * @param year the year, positive or equal to 0
+     */
     public synchronized void set(int millisOfSecond, int secondOfMinute, int minuteOfDay, Day dayOfWeek,
             int weekOfMonth, Month monthOfYear, int year) {
         this.millisOfSecond = millisOfSecond;
@@ -209,71 +297,147 @@ public class SimLifeDate implements Persistent<SimLifeDateState>, Publisher {
         computeTime();
     }
 
+    /**
+     * Set the milliseconds for the current second. All other fields remains unchanged.
+     * 
+     * @param millis the milliseconds in the range between 0 and 999
+     */
     public synchronized void setMillisOfSecond(int millis) {
         millisOfSecond = millis;
         computeTime();
     }
 
+    /**
+     * Set the seconds for the current minute. All other fields remains unchanged.
+     * 
+     * @param seconds the seconds in the range between 0 and 59
+     */
     public synchronized void setSecondOfMinute(int seconds) {
         secondOfMinute = seconds;
         computeTime();
     }
 
+    /**
+     * Set the minutes for the current day. All other fields remains unchanged.
+     * 
+     * @param minutes the minutes in the range between 0 and 19
+     */
     public synchronized void setMinuteOfDay(int minutes) {
         minuteOfDay = minutes;
         computeTime();
     }
 
+    /**
+     * Set the day for the current week. All other fields remains unchanged.
+     * 
+     * @param day the day of the week
+     */
     public synchronized void setDayOfWeek(Day day) {
         dayOfWeek = day;
         computeTime();
     }
 
+    /**
+     * Set the day for the current month. The day of week and week of month are updated. All other fields remains
+     * unchanged.
+     * 
+     * @param dayOfMonth the day in the current month in the range between 1 and 18
+     */
     public synchronized void setDayOfMonth(int dayOfMonth) {
         weekOfMonth = (int) ((dayOfMonth - 1) / DAY_PER_WEEK) + 1;
         dayOfWeek = Day.values()[(dayOfMonth - 1) % DAY_PER_WEEK];
         computeTime();
     }
 
-    public synchronized void setWeekOfMonth(int weeks) {
-        weekOfMonth = weeks;
+    /**
+     * Set the week for the current month. All other fields remains unchanged.
+     * 
+     * @param week the week in the range between 1 and 3
+     */
+    public synchronized void setWeekOfMonth(int week) {
+        weekOfMonth = week;
         computeTime();
     }
 
+    /**
+     * Set the month for the current year. All other fields remains unchanged.
+     * 
+     * @param month the month
+     */
     public synchronized void setMonthOfYear(Month month) {
         monthOfYear = month;
         computeTime();
     }
 
+    /**
+     * Set the years for the current date. All other fields remains unchanged.
+     * 
+     * @param years the years, positive or equal to 0
+     */
     public synchronized void setYear(int years) {
         year = years;
         computeTime();
     }
 
+    /**
+     * Add the given milliseconds to the current date. All the other fields are updated accordingly.
+     * 
+     * @param millis the milliseconds to add
+     */
     public synchronized void addMillis(long millis) {
         setTimeInMillis(timeInMillis + millis);
     }
 
+    /**
+     * Add the given seconds to the current date. All the other fields are updated accordingly.
+     * 
+     * @param seconds the seconds to add
+     */
     public synchronized void addSeconds(long seconds) {
         setTimeInMillis(timeInMillis + (seconds * MS_FOR_ONE_SECOND));
     }
 
+    /**
+     * Add the given minutes to the current date. All the other fields are updated accordingly.
+     * 
+     * @param minutes the minutes to add
+     */
     public synchronized void addMinutes(long minutes) {
         setTimeInMillis(timeInMillis + (minutes * MS_FOR_ONE_MINUTE));
     }
 
+    /**
+     * Add the given days to the current date. All the other fields are updated accordingly.
+     * 
+     * @param days the days to add
+     */
     public synchronized void addDays(long days) {
         setTimeInMillis(timeInMillis + (days * MS_FOR_ONE_DAY));
     }
 
+    /**
+     * Add the given weeks to the current date. All the other fields are updated accordingly.
+     * 
+     * @param weeks the weeks to add
+     */
     public synchronized void addWeeks(long weeks) {
         setTimeInMillis(timeInMillis + (weeks * MS_FOR_ONE_WEEK));
     }
 
+    /**
+     * Add the given months to the current date. All the other fields are updated accordingly.
+     * 
+     * @param months the months to add
+     */
     public synchronized void addMonths(long months) {
         setTimeInMillis(timeInMillis + (months * MS_FOR_ONE_MONTH));
     }
 
+    /**
+     * Add the given years to the current date. All the other fields are updated accordingly.
+     * 
+     * @param years the years to add
+     */
     public synchronized void addYears(long years) {
         setTimeInMillis(timeInMillis + (years * MS_FOR_ONE_YEAR));
     }
@@ -350,6 +514,11 @@ public class SimLifeDate implements Persistent<SimLifeDateState>, Publisher {
         return true;
     }
 
+    /**
+     * Format the date as described in the {@link #formatDate()} method.
+     * 
+     * @see #formatDate()
+     */
     @Override
     public String toString() {
         return formatDate();
@@ -358,39 +527,47 @@ public class SimLifeDate implements Persistent<SimLifeDateState>, Publisher {
 
 
 
-
+    @Override
     public void addSubscriber(Subscriber subscriber) {
         publisher.addSubscriber(subscriber);
     }
 
+    @Override
     public void deleteSubscriber(Subscriber subscriber) {
         publisher.deleteSubscriber(subscriber);
     }
 
+    @Override
     public void notifySubscribers() {
         publisher.notifySubscribers();
     }
 
+    @Override
     public void notifySubscribers(Object arg) {
         publisher.notifySubscribers(arg);
     }
 
+    @Override
     public void deleteSubscribers() {
         publisher.deleteSubscribers();
     }
 
+    @Override
     public void setChanged() {
         publisher.setChanged();
     }
 
+    @Override
     public void clearChanged() {
         publisher.clearChanged();
     }
 
+    @Override
     public boolean hasChanged() {
         return publisher.hasChanged();
     }
 
+    @Override
     public int countSubscribers() {
         return publisher.countSubscribers();
     }
