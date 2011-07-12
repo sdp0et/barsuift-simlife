e733ad03ddeb298c053461c66b89d93ec61712ed
cyrille.gachot <cyrille.gachot@b76370d9-c255-2e05-9e3a-6657f71092bd>
2011-07-12 09:29:46 +0000

    migrate BasicTreeTrunk3DTestHelper use to FEST assertions use

--- trunk/simLifeDisplay/src/test/java/barsuift/simLife/j3d/tree/BasicTreeTrunk3DTest.java
+++ trunk/simLifeDisplay/src/test/java/barsuift/simLife/j3d/tree/BasicTreeTrunk3DTest.java
@@ -19,18 +19,30 @@
 package barsuift.simLife.j3d.tree;
 
 import javax.media.j3d.Appearance;
+import javax.media.j3d.BoundingBox;
+import javax.media.j3d.Group;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
 import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
 
+import org.fest.assertions.Delta;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import barsuift.simLife.j3d.tree.helper.BasicTreeTrunk3DTestHelper;
 import barsuift.simLife.j3d.universe.MockUniverse3D;
 import barsuift.simLife.j3d.util.ColorConstants;
 import barsuift.simLife.tree.MockTreeTrunk;
+
+import com.sun.j3d.utils.geometry.Cylinder;
+
 import static barsuift.simLife.j3d.assertions.AppearanceAssert.assertThat;
+import static barsuift.simLife.j3d.assertions.BoundingBoxAssert.assertThat;
+import static barsuift.simLife.j3d.assertions.CylinderAssert.assertThat;
+import static barsuift.simLife.j3d.assertions.GroupAssert.assertThat;
+import static barsuift.simLife.j3d.assertions.Transform3DAssert.assertThat;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -91,19 +103,38 @@ public class BasicTreeTrunk3DTest {
         BasicTreeTrunk3D trunk3D = new BasicTreeTrunk3D(trunk3DState);
         trunk3D.init(mockUniverse3D, mockTrunk);
 
+        Cylinder trunkCylinder = trunk3D.getTrunk();
+        assertThat(trunkCylinder).hasRadius(radius);
+        assertThat(trunkCylinder).hasHeight(height);
+
+        Group trunkGroup = trunk3D.getGroup();
+        assertThat(trunkGroup).hasExactlyOneTransformGroup();
+        TransformGroup trunkTg = assertThat(trunkGroup).getSingleTransformGroup();
+
+        assertThat(trunkTg).hasExactlyOne(Cylinder.class);
+        Cylinder trunkCylinderChild = (Cylinder) trunkTg.getChild(0);
+        assertThat(trunkCylinderChild).isEqualTo(trunkCylinder);
+
+        Transform3D transform3D = new Transform3D();
+        trunkTg.getTransform(transform3D);
+        assertThat(transform3D).hasTranslation(new Vector3f(0, height / 2, 0));
+
+        Delta bigDelta = Delta.delta(0.02f);
+        Delta smallDelta = Delta.delta(0.0001f);
+
         Point3d expectedLowerBottom = new Point3d(-0.5, -2, -0.5);
         Point3d expectedUpperBottom = new Point3d(0.5, -2, 0.5);
-        Point3d expectedMovedLowerBottom = new Point3d(-0.5, 0, -0.5);
-        Point3d expectedMovedUpperBottom = new Point3d(0.5, 0, 0.5);
+        BoundingBox bottomBounds = (BoundingBox) trunkCylinder.getShape(Cylinder.BOTTOM).getBounds();
+        assertThat(bottomBounds).hasLowerBound(expectedLowerBottom, bigDelta, smallDelta, bigDelta);
+        assertThat(bottomBounds).hasUpperBound(expectedUpperBottom, bigDelta, smallDelta, bigDelta);
+
         Point3d expectedLowerTop = new Point3d(-0.5, 2, -0.5);
         Point3d expectedUpperTop = new Point3d(0.5, 2, 0.5);
-        Point3d expectedMovedLowerTop = new Point3d(-0.5, 4, -0.5);
-        Point3d expectedMovedUpperTop = new Point3d(0.5, 4, 0.5);
-        BasicTreeTrunk3DTestHelper.checkTrunk3D(trunk3D, height, radius, expectedLowerBottom, expectedUpperBottom,
-                expectedMovedLowerBottom, expectedMovedUpperBottom, expectedLowerTop, expectedUpperTop,
-                expectedMovedLowerTop, expectedMovedUpperTop);
+        BoundingBox topBounds = (BoundingBox) trunkCylinder.getShape(Cylinder.TOP).getBounds();
+        assertThat(topBounds).hasLowerBound(expectedLowerTop, bigDelta, smallDelta, bigDelta);
+        assertThat(topBounds).hasUpperBound(expectedUpperTop, bigDelta, smallDelta, bigDelta);
 
-        Appearance appearance = trunk3D.getTrunk().getAppearance();
+        Appearance appearance = trunkCylinder.getAppearance();
         assertThat(appearance).hasAmbientColor(ColorConstants.brown);
         assertThat(appearance).hasSpecularColor(new Color3f(0.05f, 0.05f, 0.05f));
         assertThat(appearance).hasDiffuseColor(new Color3f(0.15f, 0.15f, 0.15f));
